5e80a9988a0c71066bf9467f56d1d507666659b9
py095 <wanimulumeoderhwa@gmail.com>
2019-12-30 04:31:01 +0100

    navigateurs

---
+++ app/src/main/java/com/icon/agromwinda/Splash/Splash.java
@@ -0,0 +1,40 @@
+package com.icon.agromwinda.Splash;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.icon.agromwinda.R;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Thread chrono = new Thread() {
+            @Override
+            public void run(){
+                try {
+                    sleep(3000);
+
+                } catch (InterruptedException e){
+
+                    e.printStackTrace();
+
+                } finally {
+                    Intent i = new Intent(".MAINACTIVITY");
+                    startActivity(i);
+
+                }
+            }
+        };
+        chrono.start();
+
+    }
+    @Override
+    protected void onPause (){
+        super.onPause();
+        finish();
+    }
+}
